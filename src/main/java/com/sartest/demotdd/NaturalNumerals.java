e319488c027a3d3839de05573dd677003e236ee4
sergioartest <sergioar.test@gmail.com>
2019-02-14 01:47:31 -0300

    Refactor: Code cleaning

--- src/main/java/com/sartest/demotdd/NaturalNumerals.java
+++ src/main/java/com/sartest/demotdd/NaturalNumerals.java
@@ -1,25 +1,10 @@
 package com.sartest.demotdd;
-import java.util.HashMap;
 
 
 public class NaturalNumerals {
 
-	private HashMap<Integer,String[]> romanSymbols;
-	
-	public NaturalNumerals(){
-		this.romanSymbols = new HashMap<>();
-		this.romanSymbols.put(1,	new String[]{"I", "V", "X"});
-		this.romanSymbols.put(10,	new String[]{"X", "L", "C"});
-		this.romanSymbols.put(100,	new String[]{"C", "D", "M"});
-		this.romanSymbols.put(1000,	new String[]{"M", null,null});
-	}
-
 	public int convertToNatural(String roman){
-		return naturalNumber(roman,1000);
-	}
-
-	private int naturalNumber(String roman, int factor){
-		int result	  = 0;
+		int result = 0;
 		Object[][] symbols_values = {
 			{"I",1},
 			{"V",5},

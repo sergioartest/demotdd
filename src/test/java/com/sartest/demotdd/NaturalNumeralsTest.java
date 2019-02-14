f1d5d1e38d0e1280f4fd4c938915cca586710aa0
sergioartest <sergioar.test@gmail.com>
2019-02-14 02:13:14 -0300

    Refactor: Code cleaning and remove unnecesary tests cases

--- src/test/java/com/sartest/demotdd/NaturalNumeralsTest.java
+++ src/test/java/com/sartest/demotdd/NaturalNumeralsTest.java
@@ -9,61 +9,6 @@ public class NaturalNumeralsTest {
 	private NaturalNumerals naturalNumerals = new NaturalNumerals();
 
 	@Test
-	public void iToNatural(){
-		assertEquals(1,naturalNumerals.convertToNatural("I"));
-	}
-
-	@Test
-	public void iiToNatural(){
-		assertEquals(2,naturalNumerals.convertToNatural("II"));
-	}
-
-	@Test
-	public void iiiToNatural(){
-		assertEquals(3,naturalNumerals.convertToNatural("III"));
-	}
-
-	@Test
-	public void ivToNatural(){
-		assertEquals(4,naturalNumerals.convertToNatural("IV"));
-	}
-
-	@Test
-	public void vToNatural(){
-		assertEquals(5,naturalNumerals.convertToNatural("V"));
-	}
-
-	@Test
-	public void viToNatural(){
-		assertEquals(6,naturalNumerals.convertToNatural("VI"));
-	}
-
-	@Test
-	public void viiToNatural(){
-		assertEquals(7,naturalNumerals.convertToNatural("VII"));
-	}
-
-	@Test
-	public void viiiToNatural(){
-		assertEquals(8,naturalNumerals.convertToNatural("VIII"));
-	}
-
-	@Test
-	public void ixToNatural(){
-		assertEquals(9,naturalNumerals.convertToNatural("IX"));
-	}
-
-	@Test
-	public void xToNatural(){
-		assertEquals(10,naturalNumerals.convertToNatural("X"));
-	}
-
-	@Test
-	public void xviiiToNatural(){
-		assertEquals(18,naturalNumerals.convertToNatural("XVIII"));
-	}
-
-	@Test
 	public void romanUnitsToNatural(){
 		Object[][] numbers = {
 			{1,	"I"},
@@ -78,75 +23,75 @@ public class NaturalNumeralsTest {
 			{10,"X"}
 		};
 		for (Object[] fields : numbers)
-			assertEquals(("Error en "+fields[1]),fields[0],naturalNumerals.convertToNatural((String)fields[1]));
+			assertEquals(fields[0],naturalNumerals.convertToNatural((String)fields[1]));
 	}
 
 	@Test
 	public void romanTensToNatural(){
 		Object[][] numbers = {
-				{11,  "XI"},
-				{22,  "XXII"},
-				{33,  "XXXIII"},
-				{44,  "XLIV"},
-				{55,  "LV"},
-				{66,  "LXVI"},
-				{77,  "LXXVII"},
-				{88,  "LXXXVIII"},
-				{99,  "XCIX"},
-				{100, "C"}
-			};
-			for (Object[] fields : numbers)
-				assertEquals(("Error en "+fields[1]),fields[0],naturalNumerals.convertToNatural((String)fields[1]));
+			{11,  "XI"},
+			{22,  "XXII"},
+			{33,  "XXXIII"},
+			{44,  "XLIV"},
+			{55,  "LV"},
+			{66,  "LXVI"},
+			{77,  "LXXVII"},
+			{88,  "LXXXVIII"},
+			{99,  "XCIX"},
+			{100, "C"}
+		};
+		for (Object[] fields : numbers)
+			assertEquals(fields[0],naturalNumerals.convertToNatural((String)fields[1]));
 	}
 
 	@Test
 	public void romanHundredsToNatural(){
 		Object[][] numbers = {
-				{111,  "CXI"},
-				{222,  "CCXXII"},
-				{333,  "CCCXXXIII"},
-				{444,  "CDXLIV"},
-				{555,  "DLV"},
-				{666,  "DCLXVI"},
-				{777,  "DCCLXXVII"},
-				{888,  "DCCCLXXXVIII"},
-				{999,  "CMXCIX"},
-				{1000, "M"}
-			};
-			for (Object[] fields : numbers)
-				assertEquals(("Error en "+fields[1]),fields[0],naturalNumerals.convertToNatural((String)fields[1]));
+			{111,  "CXI"},
+			{222,  "CCXXII"},
+			{333,  "CCCXXXIII"},
+			{444,  "CDXLIV"},
+			{555,  "DLV"},
+			{666,  "DCLXVI"},
+			{777,  "DCCLXXVII"},
+			{888,  "DCCCLXXXVIII"},
+			{999,  "CMXCIX"},
+			{1000, "M"}
+		};
+		for (Object[] fields : numbers)
+			assertEquals(fields[0],naturalNumerals.convertToNatural((String)fields[1]));
 	}
 
 	@Test
 	public void someRomanNumbersToNatural(){
 		Object[][] numbers = {
-				{2,	   "II"},
-				{3,	   "III"},
-				{4,	   "IV"},
-				{6,	   "VI"},
-				{7,	   "VII"},
-				{8,	   "VIII"},
-				{9,	   "IX"},
-				{13,   "XIII"},
-				{19,   "XIX"},
-				{39,   "XXXIX"},
-				{40,   "XL"},
-				{45,   "XLV"},
-				{190,  "CXC"},
-				{90,   "XC"},
-				{390,  "CCCXC"},
-				{400,  "CD"},
-				{410,  "CDX"},
-				{495,  "CDXCV"},
-				{450,  "CDL"},
-				{499,  "CDXCIX"},
-				{900,  "CM"},
-				{999,  "CMXCIX"},
-				{1900, "MCM"},
-				{2018, "MMXVIII"}
-			};
-			for (Object[] fields : numbers)
-				assertEquals(("Error en "+fields[1]),fields[0],naturalNumerals.convertToNatural((String)fields[1]));
+			{2,	   "II"},
+			{3,	   "III"},
+			{4,	   "IV"},
+			{6,	   "VI"},
+			{7,	   "VII"},
+			{8,	   "VIII"},
+			{9,	   "IX"},
+			{13,   "XIII"},
+			{19,   "XIX"},
+			{39,   "XXXIX"},
+			{40,   "XL"},
+			{45,   "XLV"},
+			{190,  "CXC"},
+			{90,   "XC"},
+			{390,  "CCCXC"},
+			{400,  "CD"},
+			{410,  "CDX"},
+			{495,  "CDXCV"},
+			{450,  "CDL"},
+			{499,  "CDXCIX"},
+			{900,  "CM"},
+			{999,  "CMXCIX"},
+			{1900, "MCM"},
+			{2018, "MMXVIII"}
+		};
+		for (Object[] fields : numbers)
+			assertEquals(fields[0],naturalNumerals.convertToNatural((String)fields[1]));
 	}
 
 }
--- src/test/java/com/sartest/demotdd/RomanNumeralsTest.java
+++ src/test/java/com/sartest/demotdd/RomanNumeralsTest.java
@@ -9,61 +9,6 @@ public class RomanNumeralsTest {
 	private RomanNumerals romanNumerals = new RomanNumerals();
 
 	@Test
-	public void oneToRoman(){
-		assertEquals("I",romanNumerals.convertToRoman(1));
-	}
-
-	@Test
-	public void twoToRoman(){
-		assertEquals("II",romanNumerals.convertToRoman(2));
-	}
-
-	@Test
-	public void threeToRoman(){
-		assertEquals("III",romanNumerals.convertToRoman(3));
-	}
-
-	@Test
-	public void fourToRoman(){
-		assertEquals("IV",romanNumerals.convertToRoman(4));
-	}
-
-	@Test
-	public void fiveToRoman(){
-		assertEquals("V",romanNumerals.convertToRoman(5));
-	}
-
-	@Test
-	public void sixToRoman(){
-		assertEquals("VI",romanNumerals.convertToRoman(6));
-	}
-
-	@Test
-	public void sevenToRoman(){
-		assertEquals("VII",romanNumerals.convertToRoman(7));
-	}
-
-	@Test
-	public void eightToRoman(){
-		assertEquals("VIII",romanNumerals.convertToRoman(8));
-	}
-
-	@Test
-	public void nineToRoman(){
-		assertEquals("IX",romanNumerals.convertToRoman(9));
-	}
-
-	@Test
-	public void tenToRoman(){
-		assertEquals("X",romanNumerals.convertToRoman(10));
-	}
-
-	@Test
-	public void eighteenToRoman(){
-		assertEquals("XVIII",romanNumerals.convertToRoman(18));
-	}
-
-	@Test
 	public void unitsToRoman(){
 		Object[][] numbers = {
 			{1,	"I"},
@@ -78,75 +23,75 @@ public class RomanNumeralsTest {
 			{10,"X"}
 		};
 		for (Object[] fields : numbers)
-			assertEquals(("Error en "+fields[0]),fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
+			assertEquals(fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
 	}
 
 	@Test
 	public void tensToRoman(){
 		Object[][] numbers = {
-				{11,  "XI"},
-				{22,  "XXII"},
-				{33,  "XXXIII"},
-				{44,  "XLIV"},
-				{55,  "LV"},
-				{66,  "LXVI"},
-				{77,  "LXXVII"},
-				{88,  "LXXXVIII"},
-				{99,  "XCIX"},
-				{100, "C"}
-			};
-			for (Object[] fields : numbers)
-				assertEquals(("Error en "+fields[0]),fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
+			{11,  "XI"},
+			{22,  "XXII"},
+			{33,  "XXXIII"},
+			{44,  "XLIV"},
+			{55,  "LV"},
+			{66,  "LXVI"},
+			{77,  "LXXVII"},
+			{88,  "LXXXVIII"},
+			{99,  "XCIX"},
+			{100, "C"}
+		};
+		for (Object[] fields : numbers)
+			assertEquals(fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
 	}
 
 	@Test
 	public void hundredsToRoman(){
 		Object[][] numbers = {
-				{111,  "CXI"},
-				{222,  "CCXXII"},
-				{333,  "CCCXXXIII"},
-				{444,  "CDXLIV"},
-				{555,  "DLV"},
-				{666,  "DCLXVI"},
-				{777,  "DCCLXXVII"},
-				{888,  "DCCCLXXXVIII"},
-				{999,  "CMXCIX"},
-				{1000, "M"}
-			};
-			for (Object[] fields : numbers)
-				assertEquals(("Error en "+fields[0]),fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
+			{111,  "CXI"},
+			{222,  "CCXXII"},
+			{333,  "CCCXXXIII"},
+			{444,  "CDXLIV"},
+			{555,  "DLV"},
+			{666,  "DCLXVI"},
+			{777,  "DCCLXXVII"},
+			{888,  "DCCCLXXXVIII"},
+			{999,  "CMXCIX"},
+			{1000, "M"}
+		};
+		for (Object[] fields : numbers)
+			assertEquals(fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
 	}
 
 	@Test
 	public void someNumbersToRoman(){
 		Object[][] numbers = {
-				{2,	   "II"},
-				{3,	   "III"},
-				{4,	   "IV"},
-				{6,	   "VI"},
-				{7,	   "VII"},
-				{8,	   "VIII"},
-				{9,	   "IX"},
-				{13,   "XIII"},
-				{19,   "XIX"},
-				{39,   "XXXIX"},
-				{40,   "XL"},
-				{45,   "XLV"},
-				{190,  "CXC"},
-				{90,   "XC"},
-				{390,  "CCCXC"},
-				{400,  "CD"},
-				{410,  "CDX"},
-				{495,  "CDXCV"},
-				{450,  "CDL"},
-				{499,  "CDXCIX"},
-				{900,  "CM"},
-				{999,  "CMXCIX"},
-				{1900, "MCM"},
-				{2018, "MMXVIII"}
-			};
-			for (Object[] fields : numbers)
-				assertEquals(("Error en "+fields[0]),fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
+			{2,	   "II"},
+			{3,	   "III"},
+			{4,	   "IV"},
+			{6,	   "VI"},
+			{7,	   "VII"},
+			{8,	   "VIII"},
+			{9,	   "IX"},
+			{13,   "XIII"},
+			{19,   "XIX"},
+			{39,   "XXXIX"},
+			{40,   "XL"},
+			{45,   "XLV"},
+			{190,  "CXC"},
+			{90,   "XC"},
+			{390,  "CCCXC"},
+			{400,  "CD"},
+			{410,  "CDX"},
+			{495,  "CDXCV"},
+			{450,  "CDL"},
+			{499,  "CDXCIX"},
+			{900,  "CM"},
+			{999,  "CMXCIX"},
+			{1900, "MCM"},
+			{2018, "MMXVIII"}
+		};
+		for (Object[] fields : numbers)
+			assertEquals(fields[1],romanNumerals.convertToRoman((Integer)fields[0]));
 	}
 
 }
